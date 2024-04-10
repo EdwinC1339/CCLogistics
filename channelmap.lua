9c007e255d4fad45fb1b9a29826adfe760dfd29d
Edwin Camuy <edwin.camuy@upr.edu>
2024-04-10 01:51:26 -0400

    Updated channelmap

--- channelmap.lua
+++ channelmap.lua
@@ -1,9 +1,9 @@
 return {
-    train_psi_output = "redrouter_10.right",
+    train_psi_output = "redrouter_10.left",
     train_leave_output = "redrouter_10.front",
     recoup_output = "redrouter_10.back",
-    factory_output = "redrouter_10.left",
-    train_arrive_input = "redrouter_11.front",
-    buffer_empty_input = "redrouter_11.right",
-    collect_input = "redrouter_11.back"
+    factory_output = "redrouter_10.right",
+    train_arrive_input = "self.top",
+    buffer_stockpile_switch_input = "self.right",
+    collect_input = "self.back"
 }
